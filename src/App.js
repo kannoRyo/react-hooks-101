2782f7811c2db7158e40c010317a0217d93f6761
kanno <kannoryo@icloud.com>
2020-11-29 13:31:57 +0900

    implement object state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React , {useState} from 'react'
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState(multiState)
+
+  const increaseMoney = () => {
+    setState({
+      ...state,
+      money: state.money + 100
+    })
+  }
+
+  const decreaseMoney = () => {
+    setState({
+      ...state,
+      money: state.money - 100
+    })
+  }
+
+  const changeName = (e) =>{
+    const name = e.target.value
+    setState({
+      ...state,
+      name: name
+    })
+  }
   return (
     <>
-      <p>count:{count}</p>
-      <button onClick={() =>setCount(count + 1)}>+1</button>
-      <button onClick={() =>setCount(count - 1)}>-1</button>
-      <button onClick={()=> count % 3 === 0 ? setCount(0) : setCount(count)}>3の倍数のみ０に戻る</button>
+      <p>現在の{state.name}は、{state.money}円です。</p>
+      <button onClick={increaseMoney}>+100</button>
+      <button onClick={decreaseMoney}>-100</button>
+      <button onClick={() => setState({money: multiState.money})}>Reset</button>
+      <input type="text" value={state.name} onChange={(e) => changeName (e)}/>
     </>
   );
+}
+
+const multiState = {
+  money: 1000,
+  name: ''
 }
 
 export default App;
