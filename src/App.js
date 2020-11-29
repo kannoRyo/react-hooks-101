8c29897d92885cbdd473dd64c4c202871327aa8b
kanno <kannoryo@icloud.com>
2020-11-29 11:47:06 +0900

    my first React hook with useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
   return (
-    <div className="App">
-      This is a Template for React app
-    </div>
+    <>
+      <p>count:{count}</p>
+      <button onClick={() =>setCount(count + 1)}>+1</button>
+      <button onClick={() =>setCount(count - 1)}>-1</button>
+      <button onClick={()=> count % 3 === 0 ? setCount(0) : setCount(count)}>3の倍数のみ０に戻る</button>
+    </>
   );
 }
 
